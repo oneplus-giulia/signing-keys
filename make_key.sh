14f1fb5d9828596461af44d39df839b8e60f7300
pexcn <pexcn97@gmail.com>
2025-06-28 21:35:33 +0800

    update key subject

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,4 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=US/ST=California/L=Mountain View/O=Google/OU=Android/CN=Evolution-X/emailAddress=android@android.com'
